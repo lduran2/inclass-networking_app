2fc77927724046cdd282080056932318c551e3f1
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-25 11:41:07 -0400

    attempt to read temple

--- app/src/main/java/com/example/networkingapp/MainActivity.kt
+++ app/src/main/java/com/example/networkingapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.networkingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread {
+            val url = URI("temple.edu").toURL()
+
+            val websiteContent = url.openStream()
+                .bufferedReader().readLine()
+
+            Log.d("Website", websiteContent)
+        }
     }
 }
