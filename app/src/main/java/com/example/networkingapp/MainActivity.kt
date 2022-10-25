a72ec6676b702dbdfd44ae99225acaaab13c4eab
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-25 12:05:12 -0400

    display in text view

--- app/src/main/java/com/example/networkingapp/MainActivity.kt
+++ app/src/main/java/com/example/networkingapp/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.networkingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
+import android.widget.TextView
 import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+
+    val downloadHandler = Handler(Looper.getMainLooper()) {
+        textView.text = it.obj.toString()
+        true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textView = findViewById(R.id.textView)
         Thread {
             // port implied by protocal
             val url = URI("https://www.temple.edu").toURL()
@@ -22,11 +34,13 @@ class MainActivity : AppCompatActivity() {
                     while (readLine().let {
                         sb.append(it)
                         it != null
-                    })
+                    });
                 }
             val websiteContent = sb.toString()
 
+            downloadHandler.sendMessage(Message.obtain().apply{obj = websiteContent})
+
             Log.d("Website", websiteContent)
-        }
+        }.start()
     }
 }
